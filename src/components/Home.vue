8f587254b0cff7d45097b61c556f80394e3fd634
TulsiR <tjlangalia@gmail.com>
2018-07-25 20:07:25 -0700

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+   <Page class="page">
+        <ActionBar class="action-bar" title="Vue Mapbox Example"></ActionBar>
+        <GridLayout>
+                <Mapbox
+                    accessToken="pk.eyJ1IjoidHVsc2lyIiwiYSI6ImNqazBocTFpbDAxMzQzdHQ0aWUzNTdpam4ifQ.CoNT41zH04gseYdMKlLkeQ"
+                    mapStyle="traffic_day"
+                    latitude="37.7397"
+                    longitude="-121.4252"
+                    hideCompass="true"
+                    zoomLevel="12"
+                    showUserLocation="false"
+                    disableZoom="false"
+                    disableRotation="false"
+                    disableScroll="false"
+                    disableTilt="false"
+                    @mapReady="onMapReady($event)">
+                </Mapbox>
+        </GridLayout>
+    </Page>
+</template>
+<script>
+    import * as utils from "utils/utils";
+    export default {
+        data () {
+            return { };
+        },
+        methods: {
+            onMapReady(args) {
+                args.map.addMarkers([
+                    {
+                        lat: 37.7397,
+                        lng: -121.4252,
+                        title: "Tracy, CA",
+                        subtitle: "Home of The Polyglot Developer!",
+                        onCalloutTap: () => {
+                            utils.openUrl("https://www.thepolyglotdeveloper.com");
+                        }
+                    }
+                ]);
+            }
+        }
+    };
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'nativescript-vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+import Home from '../components/Home';
+import HelloWorld from '../components/HelloWorld';
+import Counter from '../components/Counter';
+
+const router = new VueRouter({
+  pageRouting: true,
+  routes: [
+    {
+      path: '/home',
+      component: Home,
+      meta: {
+        title: 'Home',
+      },
+    },
+    {path: '*', redirect: '/home'},
+  ],
+});
+
+router.replace('/home');
+
+module.exports = router;
